1565758bd6e530a2d45b25d170da65fd4da6c1c7
Aubree Herrion <aubreeherrion@gmail.com>
2022-10-08 10:51:16 -0500

    added redux and sagas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducer, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import {takeEvery, put} from 'redux-saga';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleWare = createSagaMiddleware();
+
+function* watcherSaga() {
+
+}
+
+const storeInstance = createStore(
+    combineReducer({
+        
+    }),
+    applyMiddleware(sagaMiddleWare,logger)
+);
+
+sagaMiddleWare.run(watcherSaga)
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
