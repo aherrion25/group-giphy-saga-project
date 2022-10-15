9b4a3a679ab7e70e98335b85097898d7ef04881a
Aubree Herrion <aubreeherrion@gmail.com>
2022-10-15 11:04:51 -0500

    fetch favs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import {takeEvery, put} from 'redux-saga';
+import {takeEvery, put} from 'redux-saga/effects';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import axios from 'axios';
@@ -17,14 +17,25 @@ const getFavs = (state = [], action) =>{
     }
 }
 
+function* fetchFavs(){
+    try{
+        const favResponse = yield axios.get('api/favorite');
+        console.log('fav response', favResponse.data);
+        yield put({type: 'SET_FAVS', payload: favResponse.data})
+    } catch(error) {
+        console.log('error in fetchFavs', error);
+    }
+}
+
 const sagaMiddleWare = createSagaMiddleware();
 
 function* watcherSaga() {
-
+    yield takeEvery('FETCH_FAVS', fetchFavs)
 }
 
 const storeInstance = createStore(
     combineReducers({
+        getFavs,
 
     }),
     applyMiddleware(sagaMiddleWare,logger)
